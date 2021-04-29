38d2f0de9876ac2aa73842a4f7b96b6b8f31f92d
Willian Luis Zuqui <willianluiszuqui@gmail.com>
2021-04-29 21:59:11 +0000

    melhoria no layout

--- src/app/produtos/mock-produtos.ts
+++ src/app/produtos/mock-produtos.ts
@@ -1,5 +1,18 @@
 import { Produto } from './produto';
 
 export const PRODUTOS: Produto[] = [
-  { id: 1, nome: 'Coca-Cola Café Plus Espresso Lata 220ml', preco: 1.79, quantidade: -1, foto: '11762556.jpg' },
+  {
+    id: 1,
+    nome: 'Coca-Cola Café Plus Espresso Lata 220ml',
+    preco: 1.79,
+    quantidade: -1,
+    foto: 'coca-cola-cafe-plus-espresso-lata-220ml.jpg',
+  },
+  {
+    id: 1,
+    nome: 'Croissant Creme',
+    preco: 1.79,
+    quantidade: -1,
+    foto: 'croissant-creme.jpg',
+  },
 ];
