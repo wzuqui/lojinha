9ffeed7b75868b6896a819000672166534ac0ad2
Willian Luis Zuqui <willianluiszuqui@gmail.com>
2021-04-29 22:09:02 +0000

    Auto-generated commit

---
+++ 4.cfbd425a9a7a4484c701.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{jkDv:function(n,o,t){"use strict";t.r(o),t.d(o,"AdminModule",function(){return c});var e=t("ofXK"),r=t("fXoL");let c=(()=>{class n{}return n.\u0275fac=function(o){return new(o||n)},n.\u0275mod=r.Gb({type:n}),n.\u0275inj=r.Fb({imports:[[e.c]]}),n})()}}]);
