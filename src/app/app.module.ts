6ca62d375e606d927dd34fbd957f9ac8362d1a36
Willian Luis Zuqui <willianluiszuqui@gmail.com>
2021-04-29 22:09:46 +0000

    sync

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { Router } from '@angular/router';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { PoStorageModule } from '@po-ui/ng-storage';
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,10 +17,7 @@ import { AuthModule } from './auth/auth.module';
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       registrationStrategy: 'registerWhenStable:30000'
-    }),
-    PoStorageModule.forRoot({
-      name: 'lojinha',
-    }),
+    })
   ],
   declarations: [
     AppComponent
