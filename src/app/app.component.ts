4be4ca58f8f095d6b8461e060debf703d3fd98e9
Willian Luis Zuqui <willianluiszuqui@gmail.com>
2021-05-01 05:15:02 +0000

    sync

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 import { faHome, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import { debounce } from 'src/debounce';
 
@@ -7,11 +8,22 @@ import { debounce } from 'src/debounce';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   ativo = false;
   faHome = faHome;
   faShoppingCart = faShoppingCart;
-  versao = '1.0.5-rc';
+  versao = '1.0.7-rc';
+
+  constructor(private updates: SwUpdate) {}
+
+  public async ngOnInit(): Promise<void> {
+    this.updates.available.subscribe(() => {
+      window.location.reload();
+    });
+    if (this.updates.isEnabled) {
+      await this.updates.checkForUpdate();
+    }
+  }
 
   public toogle(): void {
     debounce(() => {
@@ -19,4 +31,3 @@ export class AppComponent {
     });
   }
 }
-
